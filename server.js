01400306761b5c074e3fb0d707eccf2caac9bc8e
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-07-30 11:12:06 -0400

    added dependencies and connection in server.js file

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const path = require('path');
-const routes = require('./controllers');
-const hbs = exphbs.create({});
 
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
-
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
