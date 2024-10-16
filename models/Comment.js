ebd873297178b7f2bff100274789e07dc3fc8ee3
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-16 17:45:11 -0400

    added Comment model and content

---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
+          },
+          user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            },
+          },
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Comment;
+
