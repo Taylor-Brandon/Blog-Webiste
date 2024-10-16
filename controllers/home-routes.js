34c8741de127ba560b5ab8339e007b2894d0a8ce
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-16 17:29:14 -0400

    ensured that when a blog post is clicked on, the post handlebar is rendered with full information as well as the ability to comment

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { reseller } = require('googleapis/build/src/apis/reseller');
 const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
@@ -36,6 +37,28 @@ router.get('/signup', (req, res) => {
     }
     res.render('signup');
  });
+ 
+ router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this ID' });
+            return;
+        }
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            post, 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
 
