0dd7e1b02dd4d68df1b2ce0b631aca4a0a819cc0
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-21 14:09:44 -0400

    added post form to its own handlebars and added link to dashboard; added home route for post link

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,7 +42,6 @@ router.get('/signup', (req, res) => {
     if (!req.session.logged_in) {
         return res.redirect('/login');
     }
-
     try {
         const userData = await User.findByPk(req.session.user_id, {
             include: [
@@ -52,17 +51,11 @@ router.get('/signup', (req, res) => {
                 }
             ]
         });
-
-       
         if (!userData) {
             res.status(404).json({ message: 'No user found with this ID' });
             return;
         }
-
-       
         const user = userData.get({ plain: true });
-
-        
         res.render('dashboard', {
             user,
             logged_in: req.session.logged_in,
@@ -73,6 +66,15 @@ router.get('/signup', (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/postForm', (req, res) => {
+    if (!req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+        res.render('postForm');
+});
+
 
  router.get('/post/:id', async (req, res) => {
     try {
@@ -110,7 +112,6 @@ router.get('/signup', (req, res) => {
         res.status(500).json(err);
     }
 });
-
 module.exports = router;
 
 
