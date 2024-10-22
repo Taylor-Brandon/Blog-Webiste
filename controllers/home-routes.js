3e111367132f8c633a28801f9f07255ff768c898
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-22 14:30:05 -0400

    added editPost handlebars and home route to get editPost handlebar

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -103,6 +103,27 @@ router.get('/signup', (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/editPost/:id', async (req, res) => {
+    if (!req.session.logged_in) {
+        return res.redirect('/login');
+      }
+    try{
+        const postData = await Post.findByPk(req.params.id);
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!'});
+            return;
+        }
+        const post = postData.get({ plain: true});
+
+        res.render('editPost', {
+            post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
 
 
