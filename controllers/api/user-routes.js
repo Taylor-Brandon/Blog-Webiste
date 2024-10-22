6275cfb582d50421071aca34ffa96f74e2a71cfc
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-22 19:26:20 -0400

    fixed user route and ensured that post could be deleted

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -121,7 +121,7 @@ router.delete('/deletePost/:id', async (req, res) => {
       res.json(404).send('Post not found with this id!');
     }
 
-    await postData.destory();
+    await postData.destroy();
     res.status(200).json({ message: 'Post successfully deleted!'});
   } catch (err) {
     res.status(500).json({ message: 'Failed to delete post!', error: err.message });
--- public/js/delete.js
+++ public/js/delete.js
@@ -1,26 +1,28 @@
 const handleDeleteBtn = async (event) => {
     event.preventDefault();
 
-    if (event.target.id.contains('#delete-btn')) {
+    if (event.target.classList.contains('delete-btn')) {
         const postId = event.target.getAttribute('data-id');
-    }
-
-    if (postId) {
-        try {
-            const response = await fetch(`/api/users/deletePost/${postId}`, {
-                method: 'DELETE'
-            });
-            if (response.ok) {
-                alert('Deleted Post!');
-                document.location.replace('/dashboard');
-            } else {
-                alert('Failed to delete post.');
+        
+        if (postId) {
+            try {
+                const response = await fetch(`/api/users/deletePost/${postId}`, {
+                    method: 'DELETE'
+                });
+                if (response.ok) {
+                    alert('Deleted Post!');
+                    document.location.replace('/dashboard');
+                } else {
+                    alert('Failed to delete post.');
+                }
+            } catch (err) {
+                console.error('Error deleting post:', err);
+              alert('Failed to delete post due to a network error.');
             }
-        } catch (err) {
-            console.error('Error deleting post:', err);
-          alert('Failed to delete post due to a network error.');
         }
     }
+
+    
 };
 
-document.querySelector('#delete-btn').addEventListener('click', handleDeleteBtn);
+document.querySelector('.delete-btn').addEventListener('click', handleDeleteBtn);
