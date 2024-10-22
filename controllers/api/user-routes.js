bff4e85e345b779446b07023a6deccb60117ff2c
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-22 17:11:08 -0400

    added user route to delete post

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -113,5 +113,19 @@ router.post('/editPost/:id', async (req, res) => {
 }
 });
 
+router.delete('/dashboard', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (!postData) {
+      res.json(404).send('Post not found with this id!');
+    }
+
+    await postData.destory();
+    res.status(200).json({ message: 'Post successfully deleted!'});
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete post!', error: err.message });
+  }
+});
 
 module.exports = router;
