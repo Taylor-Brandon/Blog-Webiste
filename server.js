19d66c095a8aeac60aa7d62eb6f97269f2ac991b
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-22 19:35:07 -0400

    limited session time

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const hbs = exphbs.create({});
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {maxAge: 30 * 60 * 1000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
